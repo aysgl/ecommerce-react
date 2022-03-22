40b4cc400cb0e3ef934d2a6636f2a2a9274b8f7e
aysgl <aysegul.avcu@hotmail.com>
2022-03-22 23:15:54 +0300

    common

---
+++ src/store/reducer/commonReducer.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { slidedata } from "../../data/slidedata"
+
+const slice = createSlice({
+    name: "common",
+    initialState: {
+        colors: ["primary", "success", "warning"],
+        hexColors: ["#ff0984", "#0ac5b6", "#ffc107"]
+    },
+    reducers: {
+        getSlider: (sliders, action) => {
+            const product = sliders.product.find(i => i.id === Number(action.payload))
+            sliders.product = product
+        },
+    }
+})
+
+export default slice.reducer
+
+
+// Actions
+export const { getSlider, colors } = slice.actions

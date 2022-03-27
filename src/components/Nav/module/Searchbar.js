4e39cab52144ae62046236a635f4b179fa4e74c0
aysgl <aysegul.avcu@hotmail.com>
2022-03-27 13:19:54 +0300

    400 page

--- src/components/Nav/module/Searchbar.js
+++ src/components/Nav/module/Searchbar.js
@@ -1,48 +1,71 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
-import { Input, InputGroup, InputGroupText, ListGroup, ListGroupItem, UncontrolledCollapse } from 'reactstrap'
+import { Button, Input, InputGroup, InputGroupText, ListGroup, ListGroupItem, UncontrolledCollapse } from 'reactstrap'
 import Link from "next/link"
 
 export default function Searchbar({ width }) {
     const { product } = useSelector(state => state.products);
     const [filter, setFilter] = useState("")
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onChangeHandle = (e) => {
+        setFilter(e.target.value)
+        setIsOpen(true)
+    }
+    const onLeaveHandle = () => {
+        setIsOpen(false)
+    }
 
     const filtered = product.filter((item) => {
-        return Object.keys(item).some((key) =>
-            key.toString().toLowerCase().includes(filter.toLocaleLowerCase())
+        return Object.keys(item).some(() =>
+            item.title.toString().toLowerCase().includes(filter.toLocaleLowerCase())
         );
     });
-    console.log("fil", filtered);
 
     return (
         <>
             <InputGroup style={{ minWidth: width }}>
                 <Input
-                    id="toggler"
                     name="search"
                     placeholder="search"
                     type="text"
                     value={filter}
-                    onChange={(e) => setFilter(e.target.value)} />
-                <InputGroupText color='link border border-start-0'>
+                    onClick={onChangeHandle}
+                    onMouseLeave={onLeaveHandle}
+                    onChange={onChangeHandle} />
+                <InputGroupText id="toggler" color='link border border-start-0'>
                     <i className="bi bi-search"></i>
                 </InputGroupText>
             </InputGroup>
-            <UncontrolledCollapse toggler="#toggler" className='position-absolute' style={{ zIndex: 1 }}>
-                <ListGroup style={{ minWidth: width }}>
-                    {filter &&
-                        filtered.map((list, i) =>
-                            <ListGroupItem key={i} value={list.title} className='d-flex justify-content-between align-items-center p-1'>
-                                <Link href={filter}>
-                                    <a className='link-secondary text-decoration-none'>
-                                        <img src={list.img} width="60" className="img-fluid rounded me-2" />
-                                        {list.title.slice(0, 28)}...
-                                    </a>
-                                </Link>
+            <UncontrolledCollapse
+                onMouseEnter={() => setIsOpen(true)}
+                onMouseLeave={() => setIsOpen(false)}
+                toggler="#toggler"
+                isOpen={isOpen}
+                className='position-absolute' style={{ zIndex: 1 }}>
+                {filter &&
+                    <ListGroup style={{ minWidth: width }}>
+                        {filtered.length > 0 ?
+                            filtered.map((list, i) =>
+                                <ListGroupItem key={i} value={list.title} className='d-flex justify-content-between align-items-center p-1'>
+                                    <Link href={list.slug}>
+                                        <a className='link-secondary text-decoration-none'>
+                                            <img src={list.img} width="60" className="img-fluid rounded me-2" />
+                                            {list.title.slice(0, 28)}...
+                                        </a>
+                                    </Link>
+                                </ListGroupItem>
+                            ) :
+                            <ListGroupItem className='d-flex justify-content-center align-items-center p-1'>
+                                <div className='text-dark text-center p-4'>
+                                    <i className="bi bi-emoji-dizzy display-1"></i>
+                                    <p className='fs-4 fw-bold mb-0'>sorry</p>
+                                    <p>we could not find this "<strong>{filter.slice(0, 12)}...</strong>"</p>
+                                </div>
                             </ListGroupItem>
-                        )
-                    }
-                </ListGroup>
+                        }
+                    </ListGroup>
+                }
             </UncontrolledCollapse>
 
         </>
